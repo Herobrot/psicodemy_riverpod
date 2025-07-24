66e13605c83cb8dac623e070f729292c706ed199
AlanAndreUp <cosaspami7@gmail.com>
2025-07-23 19:46:33 -0600

    foro

---
+++ lib/data/datasources/forum_api_service.dart
@@ -0,0 +1,16 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../models/forum_post.dart';
+
+class ForumApiService {
+  static Future<List<ForumPost>> fetchPosts() async {
+    final response = await http.get(Uri.parse('https://api.psicodemy.com/s4/api/posts'));
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      final posts = data['data']['posts'] as List;
+      return posts.map((json) => ForumPost.fromJson(json)).toList();
+    } else {
+      throw Exception('Error al cargar los posts');
+    }
+  }
+} 
